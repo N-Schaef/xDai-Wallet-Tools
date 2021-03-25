d43ccf6d7b295aad23b4abe5f66900e5d0b7754b
Nico Schäfer <n_schaefer11@cs.uni-kl.de>
2021-03-25 08:05:03 +0100

    Add status tables

--- walletwatch/walletview/models.py
+++ walletwatch/walletview/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 from django.conf import settings
 
-# Create your models here.
+ 
+#     _____             __ _       
+#    / ____|           / _(_)      
+#   | |     ___  _ __ | |_ _  __ _ 
+#   | |    / _ \| '_ \|  _| |/ _` |
+#   | |___| (_) | | | | | | | (_| |
+#    \_____\___/|_| |_|_| |_|\__, |
+#                             __/ |
+#                            |___/ 
+ 
+class Exchange(models.Model):
+    name = models.CharField(max_length=255)
+    api = models.CharField(max_length=1000)
+    factory_address = models.CharField(max_length=255)
 
+    def __str__(self):
+        return "{}".format(self.name)
 
 class Wallet(models.Model):
     address = models.CharField(max_length=255)
@@ -16,7 +31,17 @@ class WatchWallet:
     )
   wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
 
-# One Token
+ 
+#    _____                      _ _                   
+#   |  __ \                    (_) |                  
+#   | |__) |___ _ __   ___  ___ _| |_ ___  _ __ _   _ 
+#   |  _  // _ \ '_ \ / _ \/ __| | __/ _ \| '__| | | |
+#   | | \ \  __/ |_) | (_) \__ \ | || (_) | |  | |_| |
+#   |_|  \_\___| .__/ \___/|___/_|\__\___/|_|   \__, |
+#              | |                               __/ |
+#              |_|                              |___/ 
+ 
+
 class Token(models.Model):
     address = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
@@ -25,14 +50,6 @@ class Token(models.Model):
 
     def __str__(self):
         return "{}-{} ({})".format(self.symbol,self.name,self.address)
-
-class Exchange(models.Model):
-    name = models.CharField(max_length=255)
-    api = models.CharField(max_length=1000)
-    factory_address = models.CharField(max_length=255)
-
-    def __str__(self):
-        return "{}".format(self.name)
 
 
 class LiquidityToken(models.Model):
@@ -43,3 +60,31 @@ class LiquidityToken(models.Model):
 
     def __str__(self):
         return "{}-{} ({})".format(self.token0.symbol,self.token1.symbol,self.exchange)
+
+ 
+
+
+#     _____ _        _             
+#    / ____| |      | |            
+#   | (___ | |_ __ _| |_ _   _ ___ 
+#    \___ \| __/ _` | __| | | / __|
+#    ____) | || (_| | |_| |_| \__ \
+#   |_____/ \__\__,_|\__|\__,_|___/
+                                 
+                                 
+class TokenValue(models.Model):
+    token = models.ForeignKey(Token, on_delete=models.CASCADE)
+    exchange = models.ForeignKey(Exchange, on_delete=models.CASCADE)
+    price = models.FloatField(default=0.0)
+    fetched = models.DateTimeField('fetched')
+
+class LiquidityValue(models.Model):
+    token = models.ForeignKey(Token, on_delete=models.CASCADE)
+    value = models.FloatField(default=0.0)
+    fetched = models.DateTimeField('fetched')
+
+class WalletToken(models.Model):
+    liquidity = models.ForeignKey(LiquidityToken, on_delete=models.CASCADE)
+    balance = models.CharField(max_length=250)
+    decimals = models.IntegerField()
+    fetched = models.DateTimeField('fetched')
