4a8d02886913f79299e7f3dacdc412facecc625c
Nico Schäfer <n_schaefer11@cs.uni-kl.de>
2021-04-01 12:02:07 +0200

    Temporary change home

--- walletview/urls.py
+++ walletview/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('wallets', login_required(views.IndexView.as_view()), name='wallets'),
+    path('',login_required(views.IndexView.as_view()), name='wallets'),
+    path('wallets',login_required(views.IndexView.as_view()) , name='wallets'),
     path('<int:wallet_id>', views.wallet, name='wallet'),
     path('token/<int:token_id>', views.tokens, name='tokens'),
 ]
